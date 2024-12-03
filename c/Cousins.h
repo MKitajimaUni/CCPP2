94b95ae7a8b0d5c2eae9a2de99bac6134a5f8a8d
Mitsuhiro Kitajima <104834833+MKitajimaUni@users.noreply.github.com>
2024-12-03 15:28:41 +0100

    Update Cousins.h

--- c/Cousins.h
+++ c/Cousins.h
@@ -16,9 +16,6 @@ typedef struct {
 void sortByGeburtsjahr(int length, Geschwister *geschwisterList);
 void sortByName(int length, Geschwister *geschwisterList);
 void printGeschwisterList(Geschwister *allGeschwisterList);
-int mittelgeschwisterOfLength(Geschwister *mittelgeschwisterList);
-int allGeschwisterLength(Geschwister *mittelgeschwisterList);
 Geschwister *mittelgeschwisterOf(int n, const Person *personList);
 Geschwister *getAllMittelGeschwister(void);
-int getNameNumber(const Geschwister geschwister);
 #endif //COUSINS_H
